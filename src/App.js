b4dca110c079e8e6244b510295e3b64e42673dd2
Binish Joshi <binishjoshi@protonmail.com>
2020-08-28 09:37:59 +0545

    Added header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import Sam from './containers/page/Sam';
 import Home from './containers/page/Home';
+import Header from './containers/components/Header';
 
 const App = () => {
   return (
     <div className="container">
+      <Header />
       <BrowserRouter>
         <Route path="/" exact component={Home} />
         <Route path="/sam" exact component={Sam} />
