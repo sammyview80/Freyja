371a6c962312c145a2e249bca2c1b4c4c054697c
Binish Joshi <binishjoshi@protonmail.com>
2020-08-28 10:10:02 +0545

    Fixed react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Sam from './containers/page/Sam';
 import Home from './containers/page/Home';
 import Header from './containers/components/Header';
+import Login from './containers/page/Login';
 
 const App = () => {
   return (
     <div className="container">
-      <Header />
       <BrowserRouter>
-        <Route path="/" exact component={Home} />
-        <Route path="/sam" exact component={Sam} />
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/sam" exact component={Sam} />
+          <Route path="/login" exact component={Login} />
+        </Switch>
       </BrowserRouter>      
     </div>
   )
